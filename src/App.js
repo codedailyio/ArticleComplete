5966133a6f3c050d079af146727900e49cdddde9
Jason Brown <browniefed@gmail.com>
2019-08-06 21:58:14 -0700

    Add in code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./app.css";
+import { CheckMark, Close } from "./icons";
 
 function App() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setProgress(window.scrollY / (3000 - window.innerHeight));
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [progress]);
+
+  const position = Math.max(1 - progress, 0);
+  const complete = position === 0;
+  const notMoved = position === 1;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="App"
+      style={{
+        height: "3000px",
+        background: "linear-gradient(to bottom, #1e5799 0%,#7db9e8 100%)",
+      }}
+    >
+      <div
+        style={{
+          position: "fixed",
+          top: "30px",
+          right: "30px",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: complete ? "tomato" : "#FFF",
+            position: "relative",
+            width: "100px",
+            height: "100px",
+            borderRadius: "50px",
+            transition: "all .3s ease",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {complete ? <CheckMark /> : <Close />}
+          {!notMoved && (
+            <svg
+              viewBox="0 0 50 50"
+              width="100px"
+              height="100px"
+              style={{
+                position: "absolute",
+                left: 0,
+                top: 0,
+              }}
+            >
+              <circle
+                cx={25}
+                cy={25}
+                r={22}
+                stroke="tomato"
+                fill="transparent"
+                strokeWidth="5"
+                style={{ strokeDasharray: "137.4", strokeDashoffset: 137.4 * position }}
+              />
+            </svg>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/icons.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+export const CheckMark = () => {
+  return (
+    <svg
+      viewBox="0 0 20 20"
+      width="45px"
+      height="45px"
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <g fill="#FFF">
+        <rect y={10} width={2} height={2} />
+        <rect x={2} y={12} width={2} height={2} />
+        <rect x={4} y={14} width={2} height={2} />
+        <rect x={6} y={16} width={2} height={2} />
+        <rect x={8} y={14} width={2} height={2} />
+        <rect x={10} y={12} width={2} height={2} />
+        <rect x={12} y={10} width={2} height={2} />
+        <rect x={14} y={8} width={2} height={2} />
+        <rect x={16} y={6} width={2} height={2} />
+        <rect x={18} y={4} width={2} height={2} />
+      </g>
+    </svg>
+  );
+};
+
+export const Close = () => {
+  return (
+    <svg
+      viewBox="0 0 20 20"
+      width="45px"
+      height="45px"
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <path d="M0,2.9h2.9V0H0V2.9z M2.9,5.7h2.9V2.9H2.9V5.7z M5.7,8.6h2.9V5.7H5.7V8.6z M8.6,11.4h2.9V8.6H8.6V11.4z M5.7,14.3h2.9v-2.9H5.7V14.3z M2.9,17.1h2.9v-2.9H2.9V17.1z M0,20h2.9v-2.9H0V20z M11.4,14.3h2.9v-2.9h-2.9V14.3z M14.3,17.1h2.9v-2.9h-2.9V17.1zM17.1,20H20v-2.9h-2.9V20z M11.4,8.6h2.9V5.7h-2.9V8.6z M14.3,5.7h2.9V2.9h-2.9V5.7z M17.1,2.9H20V0h-2.9V2.9z" />
+    </svg>
+  );
+};
